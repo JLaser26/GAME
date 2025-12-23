bad4f48235bcdf1b8667c8adb0052c853aa0af7e
Jai <jaikamath169@gmail.com>
2025-12-23 17:23:06 +0530

    initial after reset

--- objects.py
+++ objects.py
@@ -1,2 +1,13 @@
-class Player:
+class Inventory:
     pass
+
+class Player:
+    def __init__(self):
+        self.BASE_HP = 100
+        self.HP = self.BASE_HP
+
+        self.sprite = ""
+        self.inventory = Inventory()
+    
+    def move(self):
+        ...
