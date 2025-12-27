8c03d2754479424b50c26bc59696b6fac5ca10f8
Jai <jaikamath169@gmail.com>
2025-12-27 11:10:14 +0530

    update map logic

--- tilemap.py
+++ tilemap.py
@@ -1,16 +1,9 @@
-# tilemap.py
 import pygame
 from settings import TILESIZE
 
-MAP = [
-    "WWWWWWWWWWWWWWWW",
-    "W..............W",
-    "W....WWW.......W",
-    "W..............W",
-    "W......WW......W",
-    "W..............W",
-    "WWWWWWWWWWWWWWWW",
-]
+def load_map(filename):
+    with open(filename, "r") as f:
+        return [line.strip() for line in f.readlines()]
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, image, x, y, solid=False):
