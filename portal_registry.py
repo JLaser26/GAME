4e64f1e0697cb7b68163d3b3e566c7505d6f92ac
Jai <jaikamath169@gmail.com>
2025-12-30 13:10:59 +0530

    improved format

--- portal_registry.py
+++ portal_registry.py
@@ -1,5 +1,5 @@
 PORTAL_LINKS = {
-    1: ("city_main", "house_01"),
-    2: ("city_main", "dungeon_entrance"),
-    3: ("house_01", "city_main"),
+    1: "dungeon_entrance",
+    2: "city_main",
 }
+
